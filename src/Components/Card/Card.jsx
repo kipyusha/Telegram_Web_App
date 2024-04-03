89ec1389fb6f0021f13cbd7b4515303dcae6acb6
kipyusha <kirya16garchev@gmail.com>
2024-04-03 20:26:02 +0600

    fix

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,10 +1,28 @@
 import React, { useState } from "react";
 import "./Card.css";
 import Button from "../Button/Button";
+import burger from "../../images/burger.png";
+import pizza from "../../images/pizza.png";
+import coca from "../../images/coca.png";
+import water from "../../images/water.png";
 function Card({ food, onAdd, onRemove }) {
   const [count, setCount] = useState(0);
   const { title, image, price, id } = food;
-
+  const getImagePath = (image) => {
+    switch (image) {
+      case "burger.png":
+        return burger;
+      case "pizza.png":
+        return pizza;
+      case "coca.png":
+        return coca;
+      case "water.png":
+        return water;
+      default:
+        return burger;
+    }
+  };
+  const imagePath = getImagePath(image);
   const handleIncrement = () => {
     setCount(count + 1);
     onAdd(food);
@@ -13,7 +31,6 @@ function Card({ food, onAdd, onRemove }) {
     setCount(count - 1);
     onRemove(food);
   };
-  console.log(image);
   return (
     <div className="card" key={id}>
       <span
@@ -22,10 +39,10 @@ function Card({ food, onAdd, onRemove }) {
         {count}
       </span>
       <div className="image__container">
-        <img src={image} alt={title} />
+        <img src={imagePath} alt={title} />
       </div>
       <h4 className="card__title">
-        {title} . <span className="card__price">{price} руб.</span>
+        {title} <span className="card__price">{price} руб.</span>
       </h4>
 
       <div className="btn-container">
