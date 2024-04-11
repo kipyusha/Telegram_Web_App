19cb1da093bb01dc5a1ddda54141990142b962dd
kipyusha <kirya16garchev@gmail.com>
2024-04-11 19:15:46 +0600

    fix

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -70,7 +70,7 @@ function Cart({
 
     const webAppURL = process.env.REACT_APP_API_KEY;
     await fetch(
-      "https://chatter.salebot.pro/api/939524cc55ca5af63a34f6179099165f/save_variables",
+      "https://chatter.salebot.pro/api/80e0809a3951cf98d830103ba73e5bb6/save_variables",
       {
         method: "POST",
         headers: {
@@ -88,7 +88,7 @@ function Cart({
       }
     );
     await fetch(
-      "https://chatter.salebot.pro/api/939524cc55ca5af63a34f6179099165f/callback",
+      "https://chatter.salebot.pro/api/80e0809a3951cf98d830103ba73e5bb6/callback",
       {
         method: "post",
         body: JSON.stringify({
